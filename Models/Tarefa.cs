b9f0701a727e086ac209e974ad608e624f78ccb1
DevelynArantes <103970082+DevelynArantes@users.noreply.github.com>
2025-09-18 12:48:50 -0300

    Update Tarefa.cs

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -7,6 +7,10 @@ namespace MODULOAPI.Models
 {
     public class Tarefa
     {
-        
+        public int Id { get; set; }
+        public string Titulo { get; set; } 
+        public string Descricao { get; set; } 
+        public DateTime Data { get; set; }
+        public string Status { get; set; } = "Pendente"; 
     }
 }
